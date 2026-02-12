c5409f4218cec368c3e1f22fcab8c65f3e772661
isthatjason <354261041@educ.dpcdsb.org>
2026-02-12 13:01:24 -0500

    Done

---
+++ vars_investigate.py
@@ -0,0 +1,36 @@
+"""
+author: Jason James
+date: 02/12/2026
+Investigating Variables
+"""
+
+# Input
+name = "Jason"
+grade = 11
+favourite_exclamation = "YEOO"
+least_favourite_colour = "Glitter Yellow"
+is_cool = True
+math_test_score = 87
+sister_name = "Joanna"
+grounded = "took away my phone"
+shout = "that she is cooked"
+burst_out = "cry"
+
+slope = 7
+y_intercept = 13
+x = 3
+y = slope * x + y_intercept
+
+# Processing / Output
+print(
+    f"Hey guys, my name is {name} and I am currently in grade {grade}. "
+    + f"Yesterday, there was a spider in the classroom that made me yell {favourite_exclamation} whiile I was taking a math test with my {least_favourite_colour} pen :/ "
+    + f"One of the questions on the test was wether or not I thought Elvis was cool, so I obviously put {is_cool} to get the answer right... "
+    + f"Turns out that I only scored a {math_test_score} on the test. One of the qusetions was asking me to write the equation for a line "
+    + f"with a slope of {slope} and a y-intercept of {y_intercept} so I wrote y = {slope}x + {y_intercept}, but I did not notice the x value of {x} in the question "
+    + f"so I needed to, on the next line, say that the value of y was {y}. "
+    + f"I told my sister {sister_name} about this and she just laughed at me 🙄. "
+    + f"Later, she snitched on me about my marks to my parents and they {grounded} for 3 days. "
+    + f"I yelled at my sister {shout} and she started to {burst_out}!"
+    + "Anyway, that's the story of my math test."
+)
